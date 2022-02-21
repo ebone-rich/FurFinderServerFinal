61d968a5ec968dce69b8eb406b90c5b9c8240ed1
Ebone Rich <rich.ebone@gmail.com>
2022-02-20 22:29:49 -0500

    final push

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,15 +1,15 @@
-// let APIURL = '';
+let APIURL = '';
 
-// switch(window.location.hostname){
-//     //this is the local host name of your react app
-//     case 'localhost' || '127.0.0.1':
-//         //this is the local host name of your API
-//         APIURL = 'http://localhost:3000';
-//         break;
-//         //this is the deployed react application
-//         case 'dls-choreme-app.herokuapp.com':
-//             //this is the full url of your deployed API
-//             APIURL = 'https://dls-choreme-app.herokuapp.com'
-// }
+switch(window.location.hostname){
+    //this is the local host name of your react app
+    case 'localhost' || '127.0.0.1':
+        //this is the local host name of your API
+        APIURL = 'http://localhost:4000';
+        break;
+        //this is the deployed react application
+        case 'https://er-my-furfinderclient.herokuapp.com/':
+            //this is the full url of your deployed API
+            APIURL = 'https://er-my-furfriendserver.herokuapp.com/'
+}
 
-// export default APIURL;
+export default APIURL;
